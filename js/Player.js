505f8034290cce3b2fc4f85c6caaeb04b3e50a72
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-03-08 10:34:15 -0300

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,7 +1,6 @@
 class Player {
   constructor (){}
 
-
 getCount(){
   var playerCountRef = database.ref('playerCount');
   playerCountRef.on ("value",(data)=>{
@@ -9,10 +8,10 @@ getCount(){
 
   })
 }
-
+//metodo de atualização de contagem
 updateCount (count){
-  database.ref("/").update({
-    playerCount: count
+  database.ref("/").update({ //atualiza no banco de dados com referência no nó principal
+    playerCount: count //aumenta a contagem de jogadores conforme adicionado um novo nome
   });
 }
 }
